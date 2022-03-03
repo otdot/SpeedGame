dda8308fcff0919af1034d727288d288eedc8045
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-03-03 14:32:37 +0200

    mvp

--- app.js
+++ app.js
@@ -3,49 +3,84 @@ const stopbutton = document.querySelector("#stopbutton");
 const gamebuttons = document.querySelectorAll(".button");
 const modal = document.querySelector(".container");
 const closeModalbutton = document.querySelector("#closeModal");
+const scoreText = document.querySelector("#score p");
 
-let gameOn = false;
+let score = 0;
+let tempIndex = 0;
+let pace = 2000;
 
-const startGame = () => {
-  gameOn = true;
-  while (gameOn === true) {
-    const minNum = Math.ceil(1);
-    const maxNum = Math.floor(4);
-    buttonIndex = Math.floor(
-      Math.random() * (maxNum - minNum + minNum) + minNum
-    );
+const createIndex = () => {
+  const minNum = Math.ceil(1);
+  const maxNum = Math.floor(4);
+  return Math.floor(Math.random() * (maxNum - minNum + minNum) + minNum);
+};
+
+const timeOut = (buttonIndex) => {
+  timer = setTimeout(() => {
     gamebuttons.forEach((button) => {
       if (button.dataset.index == buttonIndex) {
-        button.classList.add("activeButton");
+        button.classList.remove("activeButton");
+        console.log(`button ${button.dataset.index} was DEACTIVATED`);
       }
     });
-
-    gameOn = false;
-  }
-  console.log("out of while loop");
-  //   while (gameOn === true) {
-  //     );
-  //     console.log(buttonIndex);
-  //   }
+    round();
+    pace -= 100;
+  }, pace);
 };
+
+const round = () => {
+  let buttonIndex = createIndex();
+  while (buttonIndex === tempIndex) {
+    buttonIndex = createIndex();
+  }
+  gamebuttons.forEach((button) => {
+    if (button.dataset.index == buttonIndex) {
+      button.classList.add("activeButton");
+    }
+  });
+  timeOut(buttonIndex);
+  tempIndex = buttonIndex;
+};
+
+const speedGame = (e) => {
+  console.log(
+    `This is from speed game. button ${e.currentTarget.dataset.index} was clicked`
+  );
+  if (e.currentTarget.classList.contains("activeButton")) {
+    score++;
+    pace -= 100;
+    scoreText.textContent = `Score : ${score} `;
+    gamebuttons.forEach((button) => button.classList.remove("activeButton"));
+    clearTimeout(timer);
+    round();
+  } else if (!e.currentTarget.classList.contains("activeButton")) {
+    stopGame();
+  }
+};
+
+const startGame = () => {
+  round();
+  gamebuttons.forEach((button) => button.addEventListener("click", speedGame));
+};
+
 const stopGame = () => {
-  gameOn = false;
   showModal();
 };
+
 const showModal = () => {
   modal.style.display = "block";
 };
 
 const closeModal = () => {
-  modal.style.display = "none";
+  window.location.reload();
 };
 
 startbutton.addEventListener("click", startGame);
 stopbutton.addEventListener("click", stopGame);
 closeModalbutton.addEventListener("click", closeModal);
 
-gamebuttons.forEach((button) =>
-  button.addEventListener("click", () =>
-    console.log(`button ${button.textContent} was clicked`)
-  )
-);
+// gamebuttons.forEach((button) =>
+//   button.addEventListener("click", () =>
+//     console.log(`button ${button.textContent} was clicked`)
+//   )
+// );
