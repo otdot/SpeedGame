dcf0099033e6281a8e393aca19b7120ad6b636e0
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-03-04 20:48:45 +0200

    added buttons to have randoms color and functionality if user misses to click a button to stop game

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const gamebuttons = document.querySelectorAll(".button");
 const modal = document.querySelector(".container");
 const closeModalbutton = document.querySelector("#closeModal");
 const scoreText = document.querySelector("#score p");
+const finalScore = document.querySelector(".stopGameBox p");
+const main = document.querySelector("main");
 
 let score = 0;
 let tempIndex = 0;
 let pace = 2000;
+let acceleration = 100;
+let timer;
+let count = 0;
+
+const randomColor = () => {
+  const red = Math.floor(Math.random() * 230);
+  const green = Math.floor(Math.random() * 230);
+  const blue = Math.floor(Math.random() * 230);
+  return `rgb(${red},${green},${blue})`;
+};
 
 const createIndex = () => {
   const minNum = Math.ceil(1);
@@ -15,16 +27,28 @@ const createIndex = () => {
   return Math.floor(Math.random() * (maxNum - minNum + minNum) + minNum);
 };
 
+const stopLoop = () => {
+  clearTimeout(timer);
+  gamebuttons.forEach((button) => {
+    button.style.backgroundColor = "#ffffff";
+    button.classList.remove("activeButton");
+  });
+};
+
 const timeOut = (buttonIndex) => {
   timer = setTimeout(() => {
     gamebuttons.forEach((button) => {
       if (button.dataset.index == buttonIndex) {
+        button.style.backgroundColor = "#ffffff";
         button.classList.remove("activeButton");
-        console.log(`button ${button.dataset.index} was DEACTIVATED`);
       }
     });
     round();
-    pace -= 100;
+    count++;
+    if (count > 2) {
+      stopGame();
+    }
+    pace -= acceleration;
   }, pace);
 };
 
@@ -35,6 +59,7 @@ const round = () => {
   }
   gamebuttons.forEach((button) => {
     if (button.dataset.index == buttonIndex) {
+      button.style.backgroundColor = randomColor();
       button.classList.add("activeButton");
     }
   });
@@ -43,27 +68,38 @@ const round = () => {
 };
 
 const speedGame = (e) => {
-  console.log(
-    `This is from speed game. button ${e.currentTarget.dataset.index} was clicked`
-  );
   if (e.currentTarget.classList.contains("activeButton")) {
     score++;
-    pace -= 100;
+    pace -= acceleration;
     scoreText.textContent = `Score : ${score} `;
-    gamebuttons.forEach((button) => button.classList.remove("activeButton"));
+    gamebuttons.forEach((button) => {
+      button.style.backgroundColor = "#ffffff";
+      button.classList.remove("activeButton");
+    });
     clearTimeout(timer);
     round();
-  } else if (!e.currentTarget.classList.contains("activeButton")) {
+  } else {
     stopGame();
   }
 };
 
 const startGame = () => {
+  stopbutton.style.display = "inline-block";
+  startbutton.style.display = "none";
   round();
   gamebuttons.forEach((button) => button.addEventListener("click", speedGame));
 };
 
 const stopGame = () => {
+  main.style.pointerEvents = "none";
+  stopLoop();
+  startbutton.style.display = "inline-block";
+  stopbutton.style.display = "none";
+  if (score < 10) finalScore.textContent = `Your final score: ${score}`;
+  else if (score > 10)
+    finalScore.textContent = `Good job, you score was: ${score}`;
+  else if (score > 50)
+    finalScore.textContent = `Great job, you score was: ${score}`;
   showModal();
 };
 
@@ -72,6 +108,8 @@ const showModal = () => {
 };
 
 const closeModal = () => {
+  main.style.pointerEvents = "auto";
+  
   window.location.reload();
 };
 
